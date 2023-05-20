7b5459594ffba321a18ce6bea30aa8653fe35f1c
Younes <103428700+v-younes-henni@users.noreply.github.com>
2023-05-20 20:31:42 +0100

    created public and private method definitions

--- setupServer.ts
+++ setupServer.ts
@@ -1,1 +1,38 @@
-export class FakeCompanyServer {}
+import {
+  Application,
+  Request,
+  Response,
+  NextFunction,
+  urlencoded,
+  json,
+} from "express";
+import { Server } from "http";
+
+export class FakeCompanyServer {
+  private app: Application;
+
+  constructor(app: Application) {
+    this.app = app;
+  }
+
+  public start(): void {
+    this.startServer(this.app);
+
+    this.standardMiddleware(this.app);
+    this.securityMiddleware(this.app);
+    this.routeMiddleware(this.app);
+
+    this.globalErrorHandler(this.app);
+  }
+
+  private startServer(app: Application): void {}
+  private startHttpServer(httpServer: Server): void {}
+
+  private standardMiddleware(app: Application): void {}
+  private securityMiddleware(app: Application): void {}
+  private routeMiddleware(app: Application): void {}
+
+  private createSocketIO(httpServer: Server): void {}
+
+  private globalErrorHandler(app: Application): void {}
+}
