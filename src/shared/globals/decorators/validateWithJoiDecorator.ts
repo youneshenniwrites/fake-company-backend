6d29649db680099d06c15c898738120921115fc6
youneshenniwrites <youneshenniwrites@gmail.com>
2023-05-26 18:33:33 +0100

    define our joi validation decorator

---
+++ src/shared/globals/decorators/validateWithJoiDecorator.ts
@@ -0,0 +1,21 @@
+import { JoiRequestValidationError } from '@global/helpers/handleErrors';
+import { Request, Response, NextFunction } from 'express';
+import { ObjectSchema } from 'joi';
+
+type validateWithJoiDecoratorType = (descriptor: PropertyDescriptor) => void;
+
+export function validateWithJoiDecorator(schema: ObjectSchema): validateWithJoiDecoratorType {
+  return (descriptor: PropertyDescriptor) => {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = async function (...args: [Request, Response, NextFunction]) {
+      const req: Request = args[0];
+      const { error } = await Promise.resolve(schema.validate(req.body));
+      if (error?.details) {
+        throw new JoiRequestValidationError(error.details[0].message);
+      }
+      return originalMethod.apply(this, args);
+    };
+    return descriptor;
+  };
+}
