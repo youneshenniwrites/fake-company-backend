b1ef6fe567e16416306c94a608f77e6c0024ac22
youneshenniwrites <youneshenniwrites@gmail.com>
2023-05-28 11:33:24 +0100

    add missing fileds to user tpyes

--- src/features/user/types/userTypes.ts
+++ src/features/user/types/userTypes.ts
@@ -23,6 +23,8 @@ export type UserDocument = Document & {
   bgImageVersion: string;
   bgImageId: string;
   profilePicture: string;
+  passwordResetToken?: string;
+  passwordResetExpires?: number | string;
   createdAt?: Date;
 };
 
